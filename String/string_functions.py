bddc37e70efb6bd37b75bc6e8de8e11310c66b04
Yash Surjuse <yashrsurjused@gmail.com>
2026-01-24 09:27:06 +0530

    Create string_functions.py

---
+++ String/string_functions.py
@@ -0,0 +1,15 @@
+s = input("Enter a string: ")
+
+print(len(s))          # Length
+print(s.lower())       # lowercase
+print(s.upper())       # uppercase
+print(s.title())       # title case
+print(s.capitalize())  # capitalize first letter
+
+#output:
+Enter a string: hello world
+11
+hello world
+HELLO WORLD
+Hello World
+Hello world
