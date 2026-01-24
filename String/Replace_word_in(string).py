79358b05e38ea37c6be127a7d5a605ddeeab5053
Yash Surjuse <yashrsurjused@gmail.com>
2026-01-24 09:32:18 +0530

    Create Replace_word_in script for word replacement
    Add a script to replace words in a given string.

---
+++ String/Replace_word_in(string).py
@@ -0,0 +1,14 @@
+s = input("Enter a string: ")
+
+old = input("Enter word to replace: ")
+new = input("Enter new word: ")
+
+print(s.replace(old, new))
+
+#output:
+Enter a string: I like Java
+Enter word to replace: Java
+Enter new word: Python
+
+I like Python
+
