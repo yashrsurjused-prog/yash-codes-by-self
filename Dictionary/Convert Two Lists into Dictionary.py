b7cefa5b3b14243a9e50b4e94a67ee6f34d44b79
Yash Surjuse <yashrsurjused@gmail.com>
2026-01-31 12:51:25 +0530

    Create Convert Two Lists into Dictionary.py

---
+++ Dictionary/Convert Two Lists into Dictionary.py
@@ -0,0 +1,8 @@
+keys = ["name", "age", "city"]
+values = ["Yash Surjuse", 19, "Nagpur"]
+
+data = dict(zip(keys, values))
+print(data)
+
+#Output:
+{'name': 'Yash Surjuse', 'age': 19, 'city': 'Nagpur'}
