cc206b0e0aaf9f296c1004d5a3a1efefaa6b16c6
Yash Surjuse <yashrsurjused@gmail.com>
2026-01-31 12:44:15 +0530

    Create Store Name & Age in Dictionary.py

---
+++ Dictionary/Store Name & Age in Dictionary.py
@@ -0,0 +1,15 @@
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+
+person = {
+    "name": name,
+    "age": age
+}
+
+print(person)
+
+#Input:
+Enter name: Yash Surjuse
+Enter age: 19
+#output:
+{'name': 'Yash Surjuse', 'age': 19}
