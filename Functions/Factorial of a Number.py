a15a6a59caf586fbc2f64030dea2cff6b304bf7e
Yash Surjuse <yashrsurjused@gmail.com>
2026-02-03 14:40:22 +0530

    Create Factorial of a Number.py

---
+++ Functions/Factorial of a Number.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i
+    return fact
+
+num = int(input("Enter number: "))
+print("Factorial =", factorial(num))
+
+#Output:
+Factorial = 120
+
