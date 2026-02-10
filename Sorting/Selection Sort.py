0608dc78f1bd2eece267a67997a26f96f1512f2d
Yash Surjuse <yashrsurjused@gmail.com>
2026-02-10 12:06:48 +0530

    Create Selection Sort.py

---
+++ Sorting/Selection Sort.py
@@ -0,0 +1,14 @@
+arr = [64, 25, 12, 22, 11]
+
+n = len(arr)
+for i in range(n):
+    min_index = i
+    for j in range(i+1, n):
+        if arr[j] < arr[min_index]:
+            min_index = j
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+
+print(arr)
+
+#output:
+[11, 12, 22, 25, 64]
